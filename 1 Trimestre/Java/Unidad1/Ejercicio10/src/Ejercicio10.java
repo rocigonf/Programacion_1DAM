b544410920ba09bcb7438a39efc48d41e0dfc61c
Rocío <rgonfaus@gmail.com>
2023-10-18 20:49:45 +0200

    Editados colores Ejercicio10

--- 1 Trimestre/Java/Unidad1/Ejercicio10/src/Ejercicio10.java
+++ 1 Trimestre/Java/Unidad1/Ejercicio10/src/Ejercicio10.java
@@ -3,16 +3,17 @@ public class Ejercicio10 {
 		
 		String ROJO = "\u001B[31m";
 		String BLANCO = "\u001B[37m";
+		String NEGRO = "\033[90m";
 		String RESET = "\u001B[0m";
 		
 		System.out.println(BLANCO + "■■■■■■■■■■■■■");
-		System.out.println(BLANCO + "■■■" + ROJO + "■■■" + BLANCO + "■" + ROJO + "■■■" + BLANCO + "■" + BLANCO + "■■" + RESET);
-		System.out.println(BLANCO + "■■" + ROJO + "■" + BLANCO + "■■■" + ROJO + "■" + BLANCO +  "■■■" + ROJO + "■" + BLANCO + "■■" + RESET);
-		System.out.println(BLANCO + "■■" + ROJO + "■" + BLANCO + "■■■■■■■" + ROJO + "■" + BLANCO + "■■" + RESET);
-		System.out.println(BLANCO + "■■■" + ROJO + "■" + BLANCO + "■■■■■" + ROJO + "■" + BLANCO + "■■■" + RESET);
-		System.out.println(BLANCO + "■■■■" + ROJO + "■" + BLANCO + "■■■" + ROJO + "■" + BLANCO + "■■■■" + RESET);
-		System.out.println(BLANCO + "■■■■■" + ROJO + "■" + BLANCO + "■" + ROJO + "■" + BLANCO + "■■■■■" + RESET);
-		System.out.println(BLANCO + "■■■■■■" + ROJO + "■" + BLANCO + "■■■■■■" + ROJO);
+		System.out.println(BLANCO + "■■■" + NEGRO + "■■■" + BLANCO + "■" + NEGRO + "■■■" + BLANCO + "■" + BLANCO + "■■" + RESET);
+		System.out.println(BLANCO + "■■" + NEGRO + "■" + ROJO + "■■■" + NEGRO + "■" + ROJO +  "■■■" + NEGRO + "■" + BLANCO + "■■" + RESET);
+		System.out.println(BLANCO + "■■" + NEGRO + "■" + ROJO + "■■■■■■■" + NEGRO + "■" + BLANCO + "■■" + RESET);
+		System.out.println(BLANCO + "■■■" + NEGRO + "■" + ROJO + "■■■■■" + NEGRO + "■" + BLANCO + "■■■" + RESET);
+		System.out.println(BLANCO + "■■■■" + NEGRO + "■" + ROJO + "■■■" + NEGRO + "■" + BLANCO + "■■■■" + RESET);
+		System.out.println(BLANCO + "■■■■■" + NEGRO + "■" + ROJO + "■" + NEGRO + "■" + BLANCO + "■■■■■" + RESET);
+		System.out.println(BLANCO + "■■■■■■" + NEGRO + "■" + BLANCO + "■■■■■■" + NEGRO);
 		System.out.println(BLANCO + "■■■■■■■■■■■■■");
 	}
 }
