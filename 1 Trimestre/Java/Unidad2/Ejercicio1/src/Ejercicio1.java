661c4b15133dfa3c77876ba0f1cea334594bb701
Rocío <rgonfaus@gmail.com>
2023-10-18 21:02:49 +0200

    Creados más ejercicios.

---
+++ 1 Trimestre/Java/Unidad2/Ejercicio1/src/Ejercicio1.java
@@ -0,0 +1,13 @@
+public class Ejercicio1 {
+    public static void main(String[] args) throws Exception {
+        //Declaramos variables x e y, valiendo respectivamente 144 y 999.
+        float x = 144;
+        float y = 999;
+        System.out.println("El valor de x es: " + x);
+        System.out.println("El valor de y es: " + y);
+        System.out.println("La suma de ambos es: " + (x + y));
+        System.out.println("La resta de ambos es: " + (x - y));
+        System.out.println("La multiplicación de ambos es: " + (x * y));
+        System.out.println("La división de ambos es: " + (x / y));
+    }
+}
---
+++ 1 Trimestre/Java/Unidad2/Ejercicio3/src/Ejercicio3.java
@@ -0,0 +1,10 @@
+public class Ejercicio3 {
+    public static void main(String[] args) throws Exception {
+        String nombre = "Rocío Gonfaus Luengo";
+        String calle = "C/Ejemplo de Calle";
+        String telefono = "+34 965965965";
+        System.out.println("¡Hola! Mi nombre es " + nombre + ".");
+        System.out.println("Vivo en " + calle + ".");
+        System.out.println("Mi número de teléfono es " + telefono + ".");
+    }
+}
